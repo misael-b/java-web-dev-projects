d44449f4689f6ebfcef4bbb9f1fe8c66b4211dc9
mbarcenas <misael.barcenas@yahoo.com>
2023-12-10 12:35:15 -0600

    completed exercises

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -9,9 +9,53 @@ public class Course {
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+    public String toString(){
+        String students = "";
+        for (Student student : enrolledStudents){
+            students += student.getName();
+        }
+        return  String.format("%s is a topic taught by %s. Current enrolled students are %s",
+        this.topic, instructor.getFirstName(), students);
+
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
+        if (toBeCompared == null) {
+            return false;
+        }
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+        Course course = (Course) toBeCompared;
+        String instructorName1 = course.getInstructor().getFirstName();
+        String topicName1 = course.getTopic();
+
+        String instructorName = getInstructor().getFirstName();
+        String topicName = getTopic();
+
+        return ((instructorName.equals(instructorName1)) && (topicName.equals(topicName1)));
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Teacher instructor) {
+        this.instructor = instructor;
+    }
 }
