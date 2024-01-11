3a58630f29f044a3a0675ce751c87815b9c317c9
mbarcenas <misael.barcenas@yahoo.com>
2024-01-10 20:02:03 -0600

    completed book exercises

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
@@ -1,5 +1,10 @@
 package org.launchcode;
 
-public class TemperatureException {
+public class TemperatureException  extends Exception{
     // Write code here!
+    public TemperatureException(String message){
+        super(message);
+    }
+
+
 }
